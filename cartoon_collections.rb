5638040d25a54cf61bc6c67a9d9cf2628a402fc2
Charlie Broom <broomc425@gmail.com>
2019-11-10 05:12:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,10 +16,12 @@ end
 
 def long_planeteer_calls(calls)# code an argument here
   # Your code here
-  calls.any? { 
+  calls.any? { |i| i.length > 4 }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheese_types)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  cheese_types.find {
 end
