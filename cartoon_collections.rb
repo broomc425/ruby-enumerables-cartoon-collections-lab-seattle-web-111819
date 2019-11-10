072e67aa416e75ee2a56913a606ecf9b9e260b99
Charlie Broom <broomc425@gmail.com>
2019-11-10 05:41:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,14 +1,13 @@
 def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
-  dwarves.each_with_index do |dwarf, index|
-    puts "#{index + 1}. #{dwarf}"
+  dwarves.each_with_index do 
   end
   
 end
 
 def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
-  planeteer_calls.collect { |i| i.capitalize << "!" }
+  
   
   #=> ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
 
@@ -16,12 +15,12 @@ end
 
 def long_planeteer_calls(calls)# code an argument here
   # Your code here
-  calls.any? { |i| i.length > 4 }
+  
 end
 
 def find_the_cheese(cheese_types)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   
-  cheese_types.find {
+  
 end
